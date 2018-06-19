fd611083bccff00a61c22334b0f9bbf21622dd9d
ilamaaa <ilamaaa@users.noreply.github.com>
2018-06-19 15:16:15 +0100

    all the stuff
    all the stuff

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # hreflang-chcker
 free crawl a domain to check hreflang tags are on the pages the way they should be
+
+
+Packages to install:
+	- urllib
+	- bs4
+	- requests
+	- time
+
+runs on python 3.x
+
+does not check sitemap implementation -- may add that in the future
---
+++ hreflang.py
@@ -0,0 +1,131 @@
+import requests
+from bs4 import BeautifulSoup
+import urllib.robotparser as robotparser
+import time
+
+
+def indexable(page, homepage):
+    raw_home = homepage if homepage[len(homepage) - 1] != "/" else homepage[:-1]
+    r = requests.get(page, allow_redirects=False)
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.content, "lxml")
+        canonicalEl = soup.find("link", {"rel": "canonical"}) if soup.find("link", {"rel": "canonical"}) is not None else ""
+        canonical = canonicalEl["href"] if canonicalEl is not "" else ""
+        if canonical == page or raw_home + canonical == page or canonical == "":
+            robotsEl = soup.find("meta", {"name":"robots"}) if soup.find("meta", {"name":"robots"}) is not None else ""
+            robots = robotsEl["content"] if robotsEl is not "" else ""
+            if "noindex" not in robots.lower() or "no index" not in robots.lower():
+                rp = robotparser.RobotFileParser()
+                rp.set_url(raw_home + "/robots.txt")
+                rp.read()
+                return [rp.can_fetch("Googlebot", page), r]
+            else:
+                return [False, r]
+        else:
+            return [False, r]
+    else:
+        return [False, r]
+
+
+def check_hreflang(page, homepage, soup):
+    is_page_ok = 0
+    checked = []
+    source_hreflang_links = list(map(lambda w: w["href"], soup.find_all("link", {"rel": "alternate"})))
+    if len(source_hreflang_links) == 0:
+        print(page + " has no hreflang links")
+        is_page_ok = is_page_ok + 1
+    try:
+        source_hreflang_targets = list(map(lambda z: z["hreflang"], soup.find_all("link", {"rel": "alternate"})))
+        if page not in source_hreflang_links:
+            print(page + " is missing self referring tag")
+            is_page_ok = is_page_ok + 1
+        for source_link in source_hreflang_links:
+            source_target = source_hreflang_targets[source_hreflang_links.index(source_link)]
+            x = indexable(source_link, homepage)
+            checked.append(source_link)
+            if x[0]:
+                soup = BeautifulSoup(x[1].content, "lxml")
+                current_hreflang_links = list(map(lambda y: y["href"], soup.find_all("link", {"rel": "alternate"})))
+                if source_link not in source_hreflang_links:
+                    print(source_link + " with target " + source_target + " is missing self referring tag")
+                    is_page_ok = is_page_ok + 1
+                else:
+                    if page not in current_hreflang_links:
+                        print(source_link + " is not pointing back to " + page)
+                        is_page_ok = is_page_ok + 1
+            else:
+                print(page + " has a tag that points to " + source_link + " which is not indexable")
+                is_page_ok = is_page_ok + 1
+        if is_page_ok == 0:
+            print(page + " is OK")
+        return checked
+    except:
+        print(page + " has link rel tag but is using it for none-hreflang stuff")
+        return checked
+
+
+
+def rec_crawl(cur, to_crawl, homepage, crawled):
+    if len(to_crawl) == 0:
+        print("All done")
+    else:
+        print(str(len(to_crawl)) + " to_crawl " + str(len(crawled)) + " crawled " + cur)
+        crawled.append(cur)
+        to_crawl.remove(cur)
+        x = indexable(cur, homepage)
+        if x[0]:
+            raw_home = homepage if homepage[len(homepage) - 1] != "/" else homepage[:-1]
+            r = x[1]
+            soup = BeautifulSoup(r.content, "lxml")
+            checked = check_hreflang(cur, homepage, soup)
+            crawled = list(set(crawled + checked))
+            to_crawl = list(set(to_crawl) - set(checked))
+            for link in soup.find_all("a"):
+                try:
+                    link = raw_home + link["href"] if link["href"] == "" or link["href"][:1] == "/" else link["href"]
+                    link = link[:link.index("#")] if "#" in link else link
+                    if link not in to_crawl and link not in crawled and link[:len(homepage)] == homepage:
+                        to_crawl.append(link)
+                    else:
+                        pass
+                except:
+                    pass
+            time.sleep(1)
+            rec_crawl(to_crawl[0], to_crawl, homepage, crawled)
+        else:
+            time.sleep(1)
+            print(cur + " is not indexable")
+            rec_crawl(to_crawl[0], to_crawl, homepage, crawled)
+
+
+def check_sitemap(homepage):
+    # is there a sitemap with hreflang if yes return false else return true.
+    return True
+
+
+def start_crawl(homepage):
+    if check_sitemap(homepage):
+        x = indexable(homepage, homepage)
+        if x[0]:
+            to_crawl = []
+            raw_home = homepage if homepage[len(homepage) - 1] != "/" else homepage[:-1]
+            r = x[1]
+            soup = BeautifulSoup(r.content, "lxml")
+            check_hreflang(homepage, homepage, soup)
+            for link in soup.find_all("a"):
+                try:
+                    link = raw_home + link["href"] if link["href"] == "" or link["href"][:1] == "/" else link["href"]
+                    link = link[:link.index("#")] if "#" in link else link
+                    if link not in to_crawl and link != homepage and link[:len(homepage)] == homepage:
+                        to_crawl.append(link)
+                    else:
+                        pass
+                except:
+                    pass
+            rec_crawl(to_crawl[0], to_crawl, homepage, [])
+        else:
+            print("your home page is not indexable why... just why???")
+
+
+input = input("gis ur domayne: ")
+start_crawl(input.strip(" "))
